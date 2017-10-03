554a22371a753018d37939c08e1084ade91e6099
Sorabh <ssorabh.ssharma@gmail.com>
2017-10-04 00:40:29 +0530

    yo meanjs and template correction

---
+++ modules/jobs/client/config/jobs.client.config.js
@@ -0,0 +1,32 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('jobs')
+    .run(menuConfig);
+
+  menuConfig.$inject = ['Menus'];
+
+  function menuConfig(menuService) {
+    // Set top bar menu items
+    menuService.addMenuItem('topbar', {
+      title: 'Jobs',
+      state: 'jobs',
+      type: 'dropdown',
+      roles: ['*']
+    });
+
+    // Add the dropdown list item
+    menuService.addSubMenuItem('topbar', 'jobs', {
+      title: 'List Jobs',
+      state: 'jobs.list'
+    });
+
+    // Add the dropdown create item
+    menuService.addSubMenuItem('topbar', 'jobs', {
+      title: 'Create Job',
+      state: 'jobs.create',
+      roles: ['user']
+    });
+  }
+}());
